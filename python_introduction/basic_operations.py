384d787fbb8cf4523f8fddb4e191f0ae0c33e795
Pandonyx <Pandonyx@gmail.com>
2025-07-08 16:23:55 +0100

    arithmetic in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+
+results1 = int(number1) + int(number2)
+results2 = int(number1) - int(number2)
+results3 = int(number1) * int(number2)
+
+print("Addition of", number1,"and",number2,"is:", results1)
+print("Subtraction of", number1,"and",number2,"is:", results2)
+print("Multiplication of", number1,"and",number2 ,"is:", results3)
