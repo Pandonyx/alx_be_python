e9fbad10e96c30149a26246f3083259aac666127
Pandonyx <Pandonyx@gmail.com>
2025-07-14 20:20:11 +0100

    typo

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,7 +3,7 @@ number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
     result = number * i
     i += 1
-    print(f"{number} x {i} = {result}")
+    print(f"{number} * {i} = {number * i}")
 
 
 
