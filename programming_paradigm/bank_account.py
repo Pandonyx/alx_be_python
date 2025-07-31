66a84123997aa2209bc92910b301c3c67f71daa7
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-07-31 05:08:21 +0100

    bank account class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(f"Current Balance:${self.balance}")
+        print(f"Current Balance: ${self.balance:.2f}")
